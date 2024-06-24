11b557286f2c41702280b734e4ce77c3fb71bf2c
Ahmadabdelwhab <ahmad.sayed.abdelwhab@gmail.com>
2024-06-25 01:19:00 +0300

    added vercel config

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from fastapi import FastAPI
 from app.api.endpoints import items
 
@@ -6,4 +9,4 @@ app = FastAPI()
 app.include_router(items.router, prefix="/api/v1")
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000)
